5613e3598cadd62c8807c6a8ec905d411da7f43b
HyunJun <shj6341@gmail.com>
2026-08-14 03:53:57 +0900

    d

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5100)
+    socketio.run(app, host='0.0.0.0', port=5100, allow_unsafe_werkzeug=True)
